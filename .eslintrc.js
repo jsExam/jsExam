a44788b392c15227303883a4de1464eb6a077e95
Felix Kästner <kaestner.fe@gmail.com>
2019-10-02 15:55:51 +0200

    Provide eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended", 
+        "plugin:react/recommended", 
+        "plugin:prettier/recommended"
+    ],
     "parserOptions": {
         "ecmaVersion": 2018
     },
